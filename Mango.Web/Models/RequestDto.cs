8f9351f3b38a3b07a6025f72bc55450ed6142745
BM-Daniel <danbabereyir@gmail.com>
2025-06-14 01:50:57 +0000

    Add project files.

---
+++ Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,9 @@
+﻿using static Mango.Web.Utility.SD;
+
+public class RequestDto
+{
+    public ApiType ApiType { get; set; } = ApiType.GET;
+    public string Url { get; set; }
+    public object Data { get; set; }
+    public string AccessToken { get; set; }
+}
---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,25 @@
+﻿namespace Mango.Web.Utility
+{
+    // SD - Static Details
+    public class SD
+    {
+        public static string CouponAPIBase { get; set; }
+        public static string ProductAPIBase { get; set; }
+        public static string AuthAPIBase { get; set; }
+        public static string ShoppingCartAPIBase { get; set; }
+
+        public const string RoleAdmin = "ADMIN";
+        public const string RoleCustomer = "CUSTOMER";
+        public const string TokenCookie = "JWTToken";
+
+
+        // an enum is used to represent a collection of related constants
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
